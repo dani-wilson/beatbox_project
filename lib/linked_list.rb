3c2d7dcae7305484feb712940e5e8073fe1f19f9
Dani Wilson <danirae.powell@gmail.com>
2023-05-12 14:57:48 -0600

    Fix functionality of append method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -5,4 +5,17 @@ class LinkedList
         self.head = nil
     end
 
+    def append(data)
+        if @head.nil?
+            self.head = Node.new(data, nil)
+        else
+            old_head = @head
+            @head = Node.new(data, old_head)
+        end
+        return @head.data
+    end
+
+
+    
+
 end
