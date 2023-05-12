4223722ab312a969a2ee3277748ffbdd8e19eeb1
Dani Wilson <danirae.powell@gmail.com>
2023-05-12 14:40:43 -0600

    Add changes to attr reader for next node

--- lib/node.rb
+++ lib/node.rb
@@ -3,7 +3,7 @@ class Node
 attr_reader :data,
             :next_node
 
-    def initialize(data)
+    def initialize(data, next_node = nil)
         @data = data
         @next_node = next_node
     end
