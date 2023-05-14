a269916971e75f99c3877271fd4f9ebba0dbd8b2
Dani Wilson <danirae.powell@gmail.com>
2023-05-14 10:03:24 -0600

    Add functioning insert method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -43,8 +43,22 @@ class LinkedList
         return data
     end
 
-
+    def insert(index, data)
+        if @head.nil?
+            new_node = Node.new(data, nil)
+            @head = new_node
+        else
+            current_node = @head
+            new_node = Node.new(data, nil)
+            (index - 1).times do
+                current_node = current_node.next_node
+            end
+            new_node.next_node = current_node.next_node
+            current_node.next_node = new_node
+        end
+        return data
+    end
 
     
-
 end
+
