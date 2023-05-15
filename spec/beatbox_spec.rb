db99b5d44ca4284620a6b126ad7e0384495f5693
Dani Wilson <danirae.powell@gmail.com>
2023-05-15 15:10:37 -0600

    Fix beatbox append test

--- spec/beatbox_spec.rb
+++ spec/beatbox_spec.rb
@@ -4,19 +4,18 @@ require './lib/beatbox'
 
 RSpec.describe Beatbox do
     bb = Beatbox.new
-    list = LinkedList.new
 
     it 'exists' do
 
         expect(bb).to be_an_instance_of(Beatbox)
-        expect(list).to be_an_instance_of(LinkedList)
+        expect(bb.list).to be_an_instance_of(LinkedList)
     end
 
     it 'can append more than one data at a time' do
         expect(bb.append("deep doo ditt")).to eq("deep doo ditt")
         expect(bb.list.head.data).to eq("deep")
-        expect(bb.list.next_node.data).to eq("doo")
-        expect(bb.append("woo hoo shu")).to eq("woo hoo shoo")
-        expect(bb.count).to eq(6)
+        expect(bb.list.head.next_node.data).to eq("doo")
+        expect(bb.append("woo hoo shu")).to eq("woo hoo shu")
+        expect(bb.list.count).to eq(6)
     end
 end
