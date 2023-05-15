a671ddd2f60198e5b2fc12d409dd2a23b74d3f6b
Dani Wilson <danirae.powell@gmail.com>
2023-05-15 14:12:04 -0600

    Add Beatbox class

---
+++ lib/beatbox.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Beatbox
+
+    attr_reader :list
+
+    def initialize
+        @list = LinkedList.new
+    end
+
+    def append(data)
+        strings = []
+        
+
+end
