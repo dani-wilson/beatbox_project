026e7fbe25e611b0ea2f3e2447e4dc884de2c1df
Dani Wilson <danirae.powell@gmail.com>
2023-05-16 15:30:08 -0600

    Add functional validation method

--- lib/beatbox.rb
+++ lib/beatbox.rb
@@ -7,13 +7,26 @@ class Beatbox
 
     def initialize
         @list = LinkedList.new
+        @defined_list = ["deep", "doo", "ditt", "wop", "shu", "shi", "tee", "dee", "boop", "la", "na", "shwop", "mm", "blop", "bop", "woo", "hoo", "wee"]
+
     end
 
 
     def append(data)
         data.split(" ").each do |data_string|
+            if @defined_list.include?(data_string)
             @list.append(data_string)
         end
+    end
+        return data
+    end
+
+    def prepend(data)
+        data.split(" ").each do |data_string|
+            if @defined_list.include?(data_string)
+            @list.prepend(data_string)
+        end
+    end
         return data
     end
 
@@ -21,18 +34,6 @@ class Beatbox
     def play  
         `say -r 174 "#{list.to_string}"`
     end
-
-
-    # def validate(data)
-    #     approved_beats = ["deep", "doo", "ditt", "wop", "shu", "shi", "tee", "dee", "boop", "la", "na", "shwop", "mm", "blop", "bop", "wee"]
-    #     to_append = []
-    #     approved_beats.each do |beats|
-    #         if data.includes.approved_beats
-    #             to_append << data
-    #         end
-    #     end
-    #     list.append(to_append)
-    # end
 
 
     def all
